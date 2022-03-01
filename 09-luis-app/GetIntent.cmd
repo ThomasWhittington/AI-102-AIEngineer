cd1627ae4a79ce485abb7af82c7034e55b879f23
TW <you@example.com>
2022-03-01 07:00:11 -0800

    09-luis-app Completed

--- 09-luis-app/GetIntent.cmd
+++ 09-luis-app/GetIntent.cmd
@@ -1,9 +1,9 @@
 @echo off
 
 rem Set values for your Language Understanding app
-set app_id=YOUR_APP_ID
-set endpoint=YOUR_ENDPOINT
-set key=YOUR_KEY
+set app_id=950275fb-ce94-48a2-80b8-cdda714462c5
+set endpoint=https://twhitt-languageunderstanding.cognitiveservices.azure.com/
+set key=9ebbd9237777447dbbc7d4805ce0e3ee
 
 rem Get parameter and encode spaces for URL
 set input=%1
