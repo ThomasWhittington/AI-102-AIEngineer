7c3b01e7da4a9fc0e1d4c63ab02e188cc86d8ef5
Your Name <you@example.com>
2022-03-01 08:04:40 -0800

    12-qna Completed

--- 12-qna/ask-question.cmd
+++ 12-qna/ask-question.cmd
@@ -2,7 +2,7 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 rem Set variables
-set prediction_url="YOUR_PREDICTION_ENDPOINT"
-set key="YOUR_KEY"
+set prediction_url="https://twhitt-language.cognitiveservices.azure.com/language/:query-knowledgebases?projectName=LearnFAQ&api-version=2021-10-01&deploymentName=production"
+set key="79a5e433fb3d40aebb6037755853178f"
 
 curl -X POST !prediction_url! -H "Ocp-Apim-Subscription-Key: !key!" -H "Content-Type: application/json" -d "{'question': 'What is a learning Path?' }"
