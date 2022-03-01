33edda7d4b604f1b40d634dd6f45be305a3a4ba5
Your Name <you@example.com>
2022-03-01 07:43:34 -0800

    08-speech-translation Completed

--- 08-speech-translation/C-Sharp/translator/Program.cs
+++ 08-speech-translation/C-Sharp/translator/Program.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using System.Media;
+using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Text;
-
-// Import namespaces
-
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech.Translation;
 
 namespace speech_translation
 {
@@ -26,16 +27,21 @@ namespace speech_translation
 
 
                 // Configure translation
-
+                translationConfig = SpeechTranslationConfig.FromSubscription(cogSvcKey, cogSvcRegion);
+                translationConfig.SpeechRecognitionLanguage = "en-US";
+                translationConfig.AddTargetLanguage("fr");
+                translationConfig.AddTargetLanguage("es");
+                translationConfig.AddTargetLanguage("hi");
+                Console.WriteLine("Ready to translate from " + translationConfig.SpeechRecognitionLanguage);
 
                 // Configure speech
-                
+                speechConfig = SpeechConfig.FromSubscription(cogSvcKey, cogSvcRegion);
 
                 string targetLanguage = "";
                 while (targetLanguage != "quit")
                 {
                     Console.WriteLine("\nEnter a target language\n fr = French\n es = Spanish\n hi = Hindi\n Enter anything else to stop\n");
-                    targetLanguage=Console.ReadLine().ToLower();
+                    targetLanguage = Console.ReadLine().ToLower();
                     if (translationConfig.TargetLanguages.Contains(targetLanguage))
                     {
                         await Translate(targetLanguage);
@@ -57,10 +63,32 @@ namespace speech_translation
             string translation = "";
 
             // Translate speech
-
+            string audioFile = "station.wav";
+            SoundPlayer wavPlayer = new SoundPlayer(audioFile);
+            wavPlayer.Play();
+            using AudioConfig audioConfig = AudioConfig.FromWavFileInput(audioFile);
+            using TranslationRecognizer translator = new TranslationRecognizer(translationConfig, audioConfig);
+            Console.WriteLine("Getting speech from file...");
+            TranslationRecognitionResult result = await translator.RecognizeOnceAsync();
+            Console.WriteLine($"Translating '{result.Text}'");
+            translation = result.Translations[targetLanguage];
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine(translation);
 
             // Synthesize translation
-
+            var voices = new Dictionary<string, string>
+            {
+                ["fr"] = "fr-FR-HenriNeural",
+                ["es"] = "es-ES-ElviraNeural",
+                ["hi"] = "hi-IN-MadhurNeural"
+            };
+            speechConfig.SpeechSynthesisVoiceName = voices[targetLanguage];
+            using SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer(speechConfig);
+            SpeechSynthesisResult speak = await speechSynthesizer.SpeakTextAsync(translation);
+            if (speak.Reason != ResultReason.SynthesizingAudioCompleted)
+            {
+                Console.WriteLine(speak.Reason);
+            }
 
         }
 
