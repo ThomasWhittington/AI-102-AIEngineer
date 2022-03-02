f90d3fccf481e209b24edfd363a1671258676668
Your Name <you@example.com>
2022-03-02 06:53:06 -0800

    16-video-indexer Completed

--- 16-video-indexer/get-videos.ps1
+++ 16-video-indexer/get-videos.ps1
@@ -1,5 +1,5 @@
-$account_id="YOUR_ACCOUNT_ID"
-$api_key="YOUR_API_KEY"
+$account_id="6f94f4f5-26b0-4a98-9fab-c172c2dd73c8"
+$api_key="0dd5d3bd43174d6297b9999090c19a89"
 $location="trial"
 
 # Call the AccessToken method with the API key in the header to get an access token
